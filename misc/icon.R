7d42c9851b44a3e5befc44c00c1f4decfc5a3925
Dustin Landers <dustin.landers@gmail.com>
2020-03-14 13:13:50 -0400

    started building out readme

---
+++ misc/icon.R
@@ -0,0 +1,14 @@
+library(hexSticker)
+
+sticker("logov2.png",
+        package = "lab",
+        p_size = 15,
+        p_color = "black",
+        p_y = 1.45,
+        s_x = 1,
+        s_y = .73,
+        s_width = .5,
+        s_height = .5,
+        h_fill = "white",
+        h_color = "black",
+        filename = "icon.png")
